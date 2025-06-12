9bb12bde31733304493b465314c9d1065ccf80c2
yuli.kamakura <barnett.yuxiang@gmail.com>
2025-06-12 13:22:50 +0800

    Adds recursive and iterative DFS traversals
    Implements both recursive and stack-based pre-order and in-order
    DFS traversals, providing immediate output for demonstration.
    Lays groundwork for post-order and level-order methods to be
    completed later. Improves clarity and testability of tree traversal logic.

--- algo/tree-1.py
+++ algo/tree-1.py
@@ -1,4 +1,3 @@
-
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -28,15 +27,101 @@ root.left.right.left = TreeNode(10)
 # DFS - pre-order; in-order; post-order;
 # BFS - level-order
 
-def dfs_preorder(node: TreeNode):
-    # 1,2,4,8,5,9,3,6,7,10
+
+# @pre-order: 1, 2, 4, 8, 9, 5, 10, 3, 6, 7
+def dfs_preorder_v1(root: TreeNode):
+    if not root:
+        return
+
+    print(root.val, end=", ")
+    if root.left:
+        dfs_preorder_v1(root.left)
+    if root.right:
+        dfs_preorder_v1(root.right)
+
+
+def dfs_preorder_v2(root: TreeNode):
+    if not root:
+        return
+
+    stack = [root]  # LIFO
+    while stack:
+        node = stack.pop()
+        print(node.val, end=", ")
+        if node.right:
+            stack.append(node.right)
+        if node.left:
+            stack.append(node.left)
+
+
+print("Pre-Order: ")
+dfs_preorder_v1(root)
+print()
+dfs_preorder_v2(root)
+print()
+print("------")
+# @pre-order
+
+
+# @in-order: 8, 4, 9, 2, 10, 5, 1, 6, 3, 7
+def dfs_inorder_v1(root: TreeNode):
+    if not root:
+        return
+
+    if root.left:
+        dfs_inorder_v1(root.left)
+    print(root.val, end=", ")
+    if root.right:
+        dfs_inorder_v1(root.right)
+
+
+def dfs_inorder_v2(root: TreeNode):
+    if not root:
+        return
+
+    stack = []
+    current = root
+    while stack or current:
+        while current:
+            stack.append(current)
+            current = current.left
+
+        current = stack.pop()
+        print(current.val, end=", ")
+
+        current = current.right
+
+
+print("In-Order: ")
+dfs_inorder_v1(root)
+print()
+dfs_inorder_v2(root)
+print()
+print("------")
+# @in-order
+
+
+# @post-order: 8, 9, 4, 10, 5, 2, 6, 7, 3, 1
+def dfs_postorder_v1(node: TreeNode):
     pass
 
-def dfs_inorder(node: TreeNode):
+
+def dfs_postorder_v1(node: TreeNode):
     pass
 
-def dfs_postorder(node: TreeNode):
-    pass
 
+print("Post-Order: ")
+dfs_postorder_v1(root)
+print()
+dfs_postorder_v1(root)
+print()
+print("------")
+# @post-order
+
+
+# @Level-order
 def bfs_levelorder(node: TreeNode):
     pass
+
+
+# @Level-order
