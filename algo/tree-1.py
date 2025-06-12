fda14c60221c31522b116a35370f7d25b367c34c
yuli.kamakura <barnett.yuxiang@gmail.com>
2025-06-12 11:05:35 +0800

    Add TreeNode class and initial tree structure with traversal function placeholders

---
+++ algo/tree-1.py
@@ -0,0 +1,41 @@
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+#         1
+#        / \
+#       2   3
+#      / \ / \
+#     4  5 6  7
+#    /    \  /
+#   8      9 10
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+root.left.left.left = TreeNode(8)
+root.left.right.right = TreeNode(9)
+root.right.right.left = TreeNode(10)
+
+# Traversal:
+# DFS - pre-order; in-order; post-order;
+# BFS - level-order
+
+def dfs_preorder(node: TreeNode):
+    pass
+
+def dfs_inorder(node: TreeNode):
+    pass
+
+def dfs_postorder(node: TreeNode):
+    pass
+
+def bfs_levelorder(node: TreeNode):
+    pass
